febb3adf6ddc4f55bc837f1a8fbb2f9566522dc0
Daniel Fässler <faesslerd@psyx.med.uni-greifswald.de>
2025-08-04 12:00:26 +0200

    Adding files for the methanol manuscript

---
+++ Scripts_Klier_Methanol/Microbiome_Modeling/run_MicrobePredictions.m
@@ -0,0 +1,15 @@
+modPath = 'C:\Users\faesslerd\Documents\Projects\Methanol_HPM\HMP_models_data_4Apples\MicrobiomeModels\Diet'
+
+resultsFolder = 'C:\Users\faesslerd\Documents\Projects\Methanol_HPM\HMP_models_data_4Apples\PredictMicrobeContributions'
+
+
+metList = {'meoh', 'xylan', 'pectin'};
+
+
+
+predictMicrobeContributions(modPath, 'resultsFoldeR', resultsFolder, 'metList', metList)
+
+
+
+
+abunFilePath, true,  'dietFilePath', dietFilePath,  'resPath', resPath)
